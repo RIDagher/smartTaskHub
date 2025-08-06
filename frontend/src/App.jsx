56f3ca90d7054051b40f4718b2b2725223a2265c
Rima Dagher <rima.t.dagher@gmail.com>
2025-08-05 21:54:44 -0400

    Scaffold frontend with vite-react-tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='container mx-auto p-4'>
+        <h1 className='text-3xl  font-bold text-black'>Welcome to Smart Task Hub</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
